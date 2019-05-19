5801b7c08a9a012a1499c12f353b95be9e632bf3
Jeff Langr <Jeff@Langrsoft.com>
2019-05-19 16:00:20 -0600

    name normalizer solution

--- src/main/scala/com/langrsoft/util/NameNormalizer.scala
+++ src/main/scala/com/langrsoft/util/NameNormalizer.scala
@@ -2,54 +2,43 @@ package com.langrsoft.util
 
 class NameNormalizer {
   def normalize(fullName: String) = {
-    throwWhenNameContainsExcessCommas(fullName)
-    val name = removeSuffix(fullName.trim())
+    throwWhenContainsExcessCommas(fullName)
+    val name = baseName(fullName.trim())
     if (isMononym(name))
       s"${name}${suffix(fullName)}"
     else
       s"${lastName(name)}, ${firstName(name)}${middleNames(name)}${suffix(fullName)}"
   }
 
-  private def throwWhenNameContainsExcessCommas(fullName: String) =
-    if (fullName.count(_ == ',') > 1)
+  private def throwWhenContainsExcessCommas(name: String) =
+    if (name.count(_ == ',') > 1)
       throw new IllegalArgumentException
 
-  private def suffixSplit(fullName: String) =
-    if (!fullName.contains(","))
-      (fullName, "")
-    else
-    {
-      val pattern = "(.*)\\,(.*)".r
-      val pattern(mainName, suffix) = fullName
-      (mainName, s",$suffix")
+  private def suffixSplit(fullName: String) = {
+    val nameCommaSuffix = "(.*)\\,(.*)".r
+    fullName match {
+      case nameCommaSuffix(name, suffix) => (name, s",$suffix")
+      case _ => (fullName, "")
     }
-
-  private def removeSuffix(fullName: String) = {
-    val (mainName, _) = suffixSplit(fullName)
-    mainName
   }
 
-  private def suffix(fullName: String) = {
-    val (_, suffix) = suffixSplit(fullName)
-    suffix
-  }
+  private def baseName(fullName: String) = suffixSplit(fullName)_1
 
-  private def parts(name: String) =
-    name.split(" ")
+  private def suffix(fullName: String) = suffixSplit(fullName)_2
+
+  private def parts(name: String) = name.split(" ")
 
   private def lastName(name: String)= {
     val nameParts = parts(name)
     nameParts(nameParts.length - 1)
   }
 
-  private def firstName(name: String) =
-    parts(name)(0)
+  private def firstName(name: String) = parts(name)(0)
 
   private def isMononym(name: String) =
-    parts(name).length == 1
+    parts(baseName(name)).length == 1
 
-  private def initial(namePart: String) =
-    s" ${namePart(0)}."
+  private def initial(namePart: String) = s" ${namePart(0)}."
 
   private def middleNames(name: String) = {
     val nameParts = parts(name)
