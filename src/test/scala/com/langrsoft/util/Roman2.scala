4e18d858c7f57c32ab1df5214e3cde8bd067625c
Jeff Langr <Jeff@Langrsoft.com>
2019-06-12 10:29:30 -0500

    fix roman table

--- src/test/scala/com/langrsoft/util/Roman2.scala
+++ src/test/scala/com/langrsoft/util/Roman2.scala
@@ -8,7 +8,12 @@ object Roman2 {
       (500 -> "D"),
       (400 -> "CD"),
       (100 -> "C"),
+      (90 -> "XC"),
+      (50 -> "L"),
+      (40 -> "XL"),
       (10 -> "X"),
+      (9 -> "IX"),
+      (5 -> "V"),
       (4 -> "IV"),
       (1 -> "I"))
   def convert(arabicIncoming: Int) = {
